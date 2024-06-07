06e5a621654f9f13f0ed182e012688b75780e509
darrenchang <darrenchang951@gmail.com>
2024-06-07 11:00:55 +0800

    Added feature to show if zsh is in a tmux session

--- .p10k.zsh
+++ .p10k.zsh
@@ -32,22 +32,12 @@
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
+    tmux_session_info       # tmux info
     os_icon                 # os identifier
+    cpu_arch
     dir                     # current directory
     vcs                     # git status
-    # =========================[ Line #2 ]=========================
-    newline                 # \n
-    prompt_char             # prompt symbol
-  )
-
-  # The list of segments shown on the right. Fill it with less important segments.
-  # Right prompt on the last prompt line (where you are typing your commands) gets
-  # automatically hidden when the input line reaches it. Right prompt above the
-  # last prompt line gets hidden if it would overlap with left prompt.
-  typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    status                  # exit code of the last command
-    command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
@@ -58,14 +48,6 @@
     nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
     nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
     nodeenv                 # node.js environment (https://github.com/ekalinin/nodeenv)
-    # node_version          # node.js version
-    # go_version            # go version (https://golang.org)
-    # rust_version          # rustc version (https://www.rust-lang.org)
-    # dotnet_version        # .NET version (https://dotnet.microsoft.com)
-    # php_version           # php version (https://www.php.net/)
-    # laravel_version       # laravel php framework version (https://laravel.com/)
-    # java_version          # java version (https://www.java.com/)
-    # package               # name@version from package.json (https://docs.npmjs.com/files/package.json)
     rbenv                   # ruby version from rbenv (https://github.com/rbenv/rbenv)
     rvm                     # ruby version from rvm (https://rvm.io)
     fvm                     # flutter version management (https://github.com/leoafarias/fvm)
@@ -106,17 +88,91 @@
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     per_directory_history   # Oh My Zsh per-directory-history local/global indicator
-    # cpu_arch              # CPU architecture
-    time                    # current time
+    time
+    command_execution_time  # duration of the last command
+    status                  # exit code of the last command
     # =========================[ Line #2 ]=========================
-    newline
-    # ip                    # ip address and bandwidth usage for a specified network interface
-    # public_ip             # public IP address
-    # proxy                 # system-wide http/https/ftp proxy
-    # battery               # internal battery
-    # wifi                  # wifi speed
-    # example               # example user-defined segment (see prompt_example function below)
+    newline                 # \n
+    prompt_char             # prompt symbol
   )
+
+  # The list of segments shown on the right. Fill it with less important segments.
+  # Right prompt on the last prompt line (where you are typing your commands) gets
+  # automatically hidden when the input line reaches it. Right prompt above the
+  # last prompt line gets hidden if it would overlap with left prompt.
+  typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()
+  #   # =========================[ Line #1 ]=========================
+  #   status                  # exit code of the last command
+  #   command_execution_time  # duration of the last command
+  #   background_jobs         # presence of background jobs
+  #   direnv                  # direnv status (https://direnv.net/)
+  #   asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
+  #   virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+  #   anaconda                # conda environment (https://conda.io/)
+  #   pyenv                   # python environment (https://github.com/pyenv/pyenv)
+  #   goenv                   # go environment (https://github.com/syndbg/goenv)
+  #   nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
+  #   nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
+  #   nodeenv                 # node.js environment (https://github.com/ekalinin/nodeenv)
+  #   # node_version          # node.js version
+  #   # go_version            # go version (https://golang.org)
+  #   # rust_version          # rustc version (https://www.rust-lang.org)
+  #   # dotnet_version        # .NET version (https://dotnet.microsoft.com)
+  #   # php_version           # php version (https://www.php.net/)
+  #   # laravel_version       # laravel php framework version (https://laravel.com/)
+  #   # java_version          # java version (https://www.java.com/)
+  #   # package               # name@version from package.json (https://docs.npmjs.com/files/package.json)
+  #   rbenv                   # ruby version from rbenv (https://github.com/rbenv/rbenv)
+  #   rvm                     # ruby version from rvm (https://rvm.io)
+  #   fvm                     # flutter version management (https://github.com/leoafarias/fvm)
+  #   luaenv                  # lua version from luaenv (https://github.com/cehoffman/luaenv)
+  #   jenv                    # java version from jenv (https://github.com/jenv/jenv)
+  #   plenv                   # perl version from plenv (https://github.com/tokuhirom/plenv)
+  #   perlbrew                # perl version from perlbrew (https://github.com/gugod/App-perlbrew)
+  #   phpenv                  # php version from phpenv (https://github.com/phpenv/phpenv)
+  #   scalaenv                # scala version from scalaenv (https://github.com/scalaenv/scalaenv)
+  #   haskell_stack           # haskell version from stack (https://haskellstack.org/)
+  #   kubecontext             # current kubernetes context (https://kubernetes.io/)
+  #   terraform               # terraform workspace (https://www.terraform.io)
+  #   # terraform_version     # terraform version (https://www.terraform.io)
+  #   aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
+  #   aws_eb_env              # aws elastic beanstalk environment (https://aws.amazon.com/elasticbeanstalk/)
+  #   azure                   # azure account name (https://docs.microsoft.com/en-us/cli/azure)
+  #   gcloud                  # google cloud cli account and project (https://cloud.google.com/)
+  #   google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
+  #   toolbox                 # toolbox name (https://github.com/containers/toolbox)
+  #   context                 # user@hostname
+  #   nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
+  #   ranger                  # ranger shell (https://github.com/ranger/ranger)
+  #   yazi                    # yazi shell (https://github.com/sxyazi/yazi)
+  #   nnn                     # nnn shell (https://github.com/jarun/nnn)
+  #   lf                      # lf shell (https://github.com/gokcehan/lf)
+  #   xplr                    # xplr shell (https://github.com/sayanarijit/xplr)
+  #   vim_shell               # vim shell indicator (:sh)
+  #   midnight_commander      # midnight commander shell (https://midnight-commander.org/)
+  #   nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
+  #   chezmoi_shell           # chezmoi shell (https://www.chezmoi.io/)
+  #   # vi_mode               # vi mode (you don't need this if you've enabled prompt_char)
+  #   # vpn_ip                # virtual private network indicator
+  #   # load                  # CPU load
+  #   # disk_usage            # disk usage
+  #   # ram                   # free RAM
+  #   # swap                  # used swap
+  #   todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
+  #   timewarrior             # timewarrior tracking status (https://timewarrior.net/)
+  #   taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
+  #   per_directory_history   # Oh My Zsh per-directory-history local/global indicator
+  #   # cpu_arch              # CPU architecture
+  #   time                    # current time
+  #   # =========================[ Line #2 ]=========================
+  #   newline
+  #   # ip                    # ip address and bandwidth usage for a specified network interface
+  #   # public_ip             # public IP address
+  #   # proxy                 # system-wide http/https/ftp proxy
+  #   # battery               # internal battery
+  #   # wifi                  # wifi speed
+  #   # example               # example user-defined segment (see prompt_example function below)
+  # )
 
   # Defines character set used by powerlevel10k. It's best to let `p10k configure` set it for you.
   typeset -g POWERLEVEL9K_MODE=nerdfont-v3
@@ -959,8 +1015,8 @@
 
   ################################[ cpu_arch: CPU architecture ]################################
   # CPU architecture color.
-  typeset -g POWERLEVEL9K_CPU_ARCH_FOREGROUND=0
-  typeset -g POWERLEVEL9K_CPU_ARCH_BACKGROUND=3
+  typeset -g POWERLEVEL9K_CPU_ARCH_FOREGROUND=232
+  typeset -g POWERLEVEL9K_CPU_ARCH_BACKGROUND=7
 
   # Hide the segment when on a specific CPU architecture.
   # typeset -g POWERLEVEL9K_CPU_ARCH_X86_64_CONTENT_EXPANSION=
@@ -1777,6 +1833,19 @@
   # Type `p10k help segment` for documentation and a more sophisticated example.
   function prompt_example() {
     p10k segment -b 1 -f 3 -i '⭐' -t 'hello, %n'
+  }
+  function prompt_tmux_session_info() {
+    # Check if zsh is in a tmux session
+    if [ -n "${TMUX}" ]; then
+      case $(tmux show -gv status) in
+        'on')
+          p10k segment -b 2 -f 0 -i ""
+          ;;
+        'off')
+          p10k segment -b 10 -f 0 -i "󱗽"
+          ;;
+      esac
+    fi
   }
 
   # User-defined prompt segments may optionally provide an instant_prompt_* function. Its job
