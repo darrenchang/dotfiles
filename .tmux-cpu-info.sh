0ff1015d3e846999131912c8bb8daee0efb5af49
Darren Chang <darrenchang951@gmail.com>
2026-04-12 22:05:34 -0400

    Refactor

--- .tmux-cpu-info.sh
+++ .tmux-cpu-info.sh
@@ -1,35 +1,70 @@
 #!/usr/bin/env bash
+# setting the locale, some users have issues with different locales, this forces the correct one
 export LC_ALL=en_US.UTF-8
 
-normalize() {
-  local label="${1}%"
-  local pad=$(( 5 - ${#label} ))
-  local left=$(( (pad + 1) / 2 ))
-  local right=$(( pad / 2 ))
-  printf "%${left}s%s%${right}s\n" "" "$label" ""
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+normalize_len() {
+  # the max length that the percent can reach, which happens for a two digit number with a decimal house: "99.9%"
+  max_len=$2
+  percent_len=${#1}
+  let diff_len=$max_len-$percent_len
+  # if the diff_len is even, left will have 1 more space than right
+  let left_spaces=($diff_len+1)/2
+  let right_spaces=($diff_len)/2
+  printf "%${left_spaces}s%s%${right_spaces}s\n" "" $1 ""
 }
 
-case "$(uname -s)" in
-  Linux)
-    CACHE=/tmp/tmux-cpu-cache
-    curr=$(awk '/^cpu / {print $2+$3+$4+$5+$6+$7+$8, $5}' /proc/stat)
-    prev=$(cat "$CACHE" 2>/dev/null)
-    echo "$curr" > "$CACHE"
+get_percent()
+{
+  case $(uname -s) in
+    Linux)
+      percent=$(LC_NUMERIC=en_US.UTF-8 top -bn2 -d 0.01 | grep "[C]pu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+      normalize_len $percent 5
+      ;;
 
-    if [ -z "$prev" ]; then
-      printf "  N/A  \n"; exit
-    fi
+    Darwin)
+      cpuvalue=$(ps -A -o %cpu | awk -F. '{s+=$1} END {print s}')
+      cpucores=$(sysctl -n hw.logicalcpu)
+      cpuusage=$(( cpuvalue / cpucores ))
+      percent="$cpuusage"
+      normalize_len $percent 5
+      ;;
 
-    read -r total1 idle1 <<< "$prev"
-    read -r total2 idle2 <<< "$curr"
-    total=$(( total2 - total1 ))
-    idle=$(( idle2 - idle1 ))
-    [ "$total" -eq 0 ] && percent=0 || percent=$(( (total - idle) * 100 / total ))
-    normalize "$percent"
+    OpenBSD)
+      cpuvalue=$(ps -A -o %cpu | awk -F. '{s+=$1} END {print s}')
+      cpucores=$(sysctl -n hw.ncpuonline)
+      cpuusage=$(( cpuvalue / cpucores ))
+      percent="$cpuusage"
+      normalize_len $percent 5
+      ;;
+
+    CYGWIN*|MINGW32*|MSYS*|MINGW*)
+      # TODO - windows compatability
+      ;;
+  esac
+}
+
+get_load() {
+  case $(uname -s) in
+  Linux | Darwin | OpenBSD)
+    loadavg=$(uptime | awk -F'[a-z]:' '{ print $2}' | sed 's/,//g')
+    echo $loadavg
     ;;
 
-  Darwin)
-    percent=$(top -l 1 -s 0 | awk '/CPU usage/ {gsub(/%/,""); print int($3+$5)}')
-    normalize "$percent"
+  CYGWIN* | MINGW32* | MSYS* | MINGW*)
+    # TODO - windows compatability
     ;;
-esac
+  esac
+}
+
+main() {
+  # storing the refresh rate in the variable RATE, default is 5
+  cpu_percent=$(get_percent)
+  percent=$(printf '%.0f' $cpu_percent)
+  percent=$(normalize_len $percent 3)
+  echo "$percent%"
+}
+
+# run main driver
+main
